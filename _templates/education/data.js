d2ca4af7bb7b8f61904c2abce3c6f4583819a0aa
Jay W Johnson <jwj@boun.cr>
2015-09-11 23:09:00 +0800

    Refactor: Edu section from hard-coded to MVVM+Ajax
    Use Ajax to load education data from json file and Knockout to create
    modals "foreach" one.

---
+++ _templates/education/data.js
@@ -0,0 +1,82 @@
+{
+  "_note": "TIP: Turn on WORD WRAP to edit paragraphs.",
+  "schools": [
+    {
+      "school": "UCSB",
+      "img": {
+        "src": "img/ucsbwave_140.jpg",
+        "width": "100"
+      },
+      "title": "University of California, Santa Barbara",
+      "subtitle": "Department of Music",
+      "highlights": "<h4>Highlights</h4>            <ul>                <li>BA in Music Theory</li>                <li>9 Math and Java Courses</li>                <li>6 <strong>French</strong> and <strong>German</strong> Courses</li>                <li>Member of <strong>Fencing</strong> Club</li>                <li>Percussion Ensemble (Marimba)</li>                <li>Cappella Choir (Baritone)</li>            </ul>",
+      "description": "I excelled in mathematics in high school and was accepted to the <strong>physics</strong> department at UCSB. I took many math and science courses but having played piano and flute since fifth grade I was also interested in music. I became more and more involved in the music department and eventually walked away with a B.A. degree in <strong>music theory</strong>.    <br><br>    I started learning <strong>Java</strong> programming during this time. The emphasis was on basic <strong>data structures</strong> and <strong>sequential data files</strong>. In my free time I was an avid <strong>marimbist</strong> and studied <strong>fencing</strong>."
+    },
+    {
+      "school": "NCKU",
+      "img": {
+        "src": "img/NCKU_logoword_140.png",
+        "width": "140"
+      },
+      "title": "National Cheng Kung University, Taiwan",
+      "subtitle": "Department of Earth Sciences",
+      "highlights": "<h4>Notable Courses</h4>            <ul>                <li>Image Processing (with IDL)</li>                <li>Remote Sensing Imagery Analysis</li>                <li>Remote Sensing of Ocean Color</li>                <li>Data Reduction (with IDL)</li>                <li>Numerical Analysis</li>                <li>Fluid Dynamics (CFD)</li>                <li>2D Shallow Water Modeling</li>                <li>Geographic Information Science (GIS)</li>                <li>Ocean Wind Wave Analysis</li>                <li>Observational Seismology</li>            </ul>",
+      "description": "I left the Navy with an interest in <strong>satellite imagery</strong> and programming. I was excited to be accepted to the Earth Sciences department and to join the <strong>remote sensing laboratory</strong>. I dove into <strong>IDL</strong> programming and using <strong>ENVI</strong> software. I also started to teach myself <strong>Python</strong> at this time and how to use it for <strong>ArcGIS</strong> scripts, image processing and many other tasks.    <br><br>    My <em><strong>thesis project</strong></em> was to write software to effectively use a car-mounted panoramic camera (Ladybug3 Spherical Camera) in analyzing landslide imagery. The goal was to use <strong>GPS</strong> and structure-from-motion (<strong>SFM</strong>) techniques to estimate landslide scale from video images and to process multiple videos to make a <strong>time-series</strong> of landslide imagery."
+    },
+    {
+      "school": "HPU",
+      "img": {
+        "src": "img/HPU_symbol.png",
+        "width": "78"
+      },
+      "title": "Hawai'i Pacific University",
+      "subtitle": "Department of Computer Science",
+      "highlights": "<h4>Courses Include:</h4>            <ul>                <li>Statistics</li>                <li>Data Structures</li>                <li>C#</li>                <li>Discrete Mathematics</li>                <li>Calculus</li>            </ul>",
+      "description": "    While in the Navy, I was stationed on Oahu and I took nine courses in math and    programming at HPU in my free time. Most classes were taught using <strong>Java</strong>    and emphasized documentation with the Unified Modeling Language (<strong>UML</strong>)."
+    },
+    {
+      "school": "DLIFLC",
+      "img": {
+        "src": "img/DLI_shield_100.png",
+        "width": "78"
+      },
+      "title": "Defense Language Institute",
+      "subtitle": "Foreign Language Center",
+      "highlights": "<h4>Highlights</h4>            <ul>                <li>Commencement Speaker</li>                <li>AA Degree in Chinese Mandarin</li>                <li>Top score on exit exam</li>                <li>Marksmanship Medal (M9 Pistol)</li>                <li>Peer tutoring</li>            </ul>",
+      "description": "After getting a B.A. in Music Theory, I saw an ad about becoming a <strong>linguist</strong> for the <strong>U.S. Navy</strong> that required nothing but an aptitude for learning languages and of course being within physical fitness requirements. Get <em>paid to learn</em> a language,  get in shape and <em>serve my country</em> all sounded great. I decided pretty quickly that I would join and pour myself into studying <strong>Chinese Mandarin</strong>. I finished at the top of my class and gave the <strong>commencement speech</strong> in Chinese.    <br><br>    I regularly tutored my peers and also compiled study materials, distributing disks to anyone who wanted them."
+    },
+    {
+      "school": "UDAC",
+      "img": {
+        "src": "img/udacity_symbol.jpeg",
+        "width": "78"
+      },
+      "title": "Udacity",
+      "subtitle": "Front-End Web Developer Nanodegree<br>Full Stack Web Developer Nanodegree",
+      "highlights": "<h4>Topics Studied</h4><ul>    <li>Artificial Intelligence for Robotics</li>    <li>Website Performance Optimization</li>    <li>Object-Oriented JavaScript</li>    <li>Bootstrap</li>    <li>KnockoutJS</li>    <li>Grunt</li>    <li>Flask + SQLAlchemy</li>    <li>Linux + Apache2 Server</li>    <li>Server Configuration</li></ul>",
+      "description": "I completed Udacity's <strong>Artificial Intelligence</strong> for Robotics course while in graduate school in Taiwan. It covered automated localization and mapping algorithms (<strong>SLAM, A<superscript>*</superscript></strong>) which was applicable to my research project. <br><br> Later, I was developing a database and business application for a chemical company and decided to advance my <strong>data-driven web</strong> skills by completing both of Udacity's <em>Front-End Developer</em> and <em>Full Stack Developer</em> series of courses and projects."
+    },
+    {
+      "school": "UMUC",
+      "img": {
+        "src": "img/umuc_logo.gif",
+        "width": "110"
+      },
+      "title": "University of Maryland, University College",
+      "subtitle": "Financial Management and Information Systems",
+      "highlights": "<h4>Courses</h4><ul>    <li>Principles of Accounting I, II</li>    <li>Accounting Software</li>    <li>Taxation of Individuals</li>    <li>Security Software and Hardware Assurance</li>    <li>Financial Decision Making for Managers</li>    <li>Financial Management for Organizations</li></ul>",
+      "description": "While in the U.S. Navy, there was a time when I was interested in studying finance. I enrolled in a <strong>Master of Science</strong> program and took a few courses in <strong>accounting software</strong> and <strong>financial management</strong>."
+    },
+    {
+      "school": "JHU",
+      "img": {
+        "src": "img/johns_symbol.png",
+        "width": "78"
+      },
+      "title": "Johns Hopkins University",
+      "subtitle": "Data Science Specialization",
+      "highlights": "<h4>Courses Completed</h4><ul>    <li>Intro to Data Science and Tools</li>    <li>R Programming</li>    <li>Getting and Cleaning Data</li>    <li>Exploratory Data Analysis</li>    <li>Reproducible Research</li>    <li>Statistical Inference</li>    <li>Regression Models</li></ul>",
+      "description": "I enjoy <strong>analysis</strong> and <strong>statistics</strong>. When I found a data science series of ten courses offered by Johns Hopkins University on <strong>Coursera</strong>, I decided to enroll and see if the field is a good fit for me. All classes used the <strong>R</strong> programming language and required producing reports with <strong>RMarkdown</strong>. I've completed 7 out of 10 courses and enjoyed them so far but I need to find time between work and two kids to complete the last three."
+}
+]
+}
